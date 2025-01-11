51e249d9e1d8516f316ffbb169b83cf796e277f2
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-01-10 21:08:17 -0500

    Pushed test files to github

--- create_valid_user.js
+++ create_valid_user.js
@@ -65,8 +65,8 @@ async function testEmailWithMailSlurp() {
     //     // const otp = otpMatch ? otpMatch[0] : null;
     //     // console.log("Extracted OTP:", otp);
 
+    console.log("DEvins")
 
-    
     } catch (error) {
         console.error("Error with MailSlurp:", error);
     }
