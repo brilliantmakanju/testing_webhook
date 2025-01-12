9baff1c22b1324766b4d9e4e69fc0a6b091d941a
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-01-12 12:30:45 -0500

    fix stuff lol: so i changed some code and added a new function i think it fixes the bug (idk tho). also moved a file cuz it looked messy and deleted something i forgot what it was but it's fine. works now. I think. pls check.

--- test_email_html_links.js
+++ test_email_html_links.js
@@ -29,6 +29,9 @@ async function testEmailWithMailSlurp() {
 
         console.log("Email sent to:", inbox.emailAddress);
 
+
+
+        
         const email = await mailslurp.waitForLatestEmail(inbox.id, 30000);
         console.log("Email body:", email.body);
 
